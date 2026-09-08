7b2df6a83100d37b4160fecdee118e7d50d8e811
takers03 <109911902+takers03@users.noreply.github.com>
2026-09-09 03:51:29 +0930

    Update inverse_kinematics_solver4.py

--- inverse_kinematics_solver4.py
+++ inverse_kinematics_solver4.py
@@ -126,46 +126,31 @@ def solver(prev_angles, pos, rot, a_n, d_n, b, tp, t1_v, t2_v):
 
     return [Th1,Th2,Th3,Th4,Th5,Th6]
 
-
-def choose_best_ik(prev_angles, pos, rot, a_n, d_n, b, tp):
-    combs1 = [1,0,1,0]
-    combs2 = [0,0,1,1]
-
-    ik_solutions = []
-
-    for i in range(4):
-        solved_ik = solver(prev_angles, pos, rot, a_n, d_n, b, tp, combs2[i], combs1[i])
-        # solved_ik = [x * 180/pi for x in solved_ik]
-
-        ik_solutions.append(solved_ik)
-
-    best_solution = None
-    min_rotation = float('inf')
-
-    for solution in ik_solutions:
-        total_rotation = np.sum(np.abs(np.array(solution) - np.array(prev_angles)))
-        if total_rotation < min_rotation:
-            min_rotation = total_rotation
-            best_solution = solution
-    
-    return best_solution
-
 def normalize_angle(angle, reference_angle):
     # Normalize angle to be closest to reference_angle, accounting for 2π periodicity
     return angle - 2 * np.pi * np.round((angle - reference_angle) / (2 * np.pi))
 
-def normalize_joint_angles(current_angles, new_angles):
-    # Normalize all joint angles to be close to current angles
-    return np.array([normalize_angle(new, ref) for new, ref in zip(new_angles, current_angles)])
+wrap_mask = [True, True, True, True, True, True]  # e.g., only wrap joints 1-5
+
+def normalize_joint_angles(current_angles, new_angles, wrap_mask):
+    normalized = []
+    for i, (new, ref) in enumerate(zip(new_angles, current_angles)):
+        if wrap_mask[i]:
+            normalized.append(normalize_angle(new, ref))
+        else:
+            normalized.append(new)  # preserve absolute angle
+    return np.array(normalized)
 
 def choose_best_ik(prev_angles, pos, rot, a_n, d_n, b, tp):
-    combs1 = [1, 0, 1, 0]
+    # Solution to replicate human arm motion (Selects elbow-down solution)
+    combs1 = [1, 0, 1, 0] 
     combs2 = [0, 0, 1, 1]
 
     ik_solutions = []
 
     for i in range(4):
         solved_ik = solver(prev_angles, pos, rot, a_n, d_n, b, tp, combs2[i], combs1[i])
+        # print(np.rad2deg(solved_ik))
         ik_solutions.append(solved_ik)
 
     best_solution = None
@@ -173,7 +158,7 @@ def choose_best_ik(prev_angles, pos, rot, a_n, d_n, b, tp):
 
     for solution in ik_solutions:
         # Normalize solution angles to be close to prev_angles
-        normalized_solution = normalize_joint_angles(prev_angles, solution)
+        normalized_solution = normalize_joint_angles(prev_angles, solution, wrap_mask)
         # Compute total rotation using normalized angles
         total_rotation = np.sum(np.abs(np.array(normalized_solution) - np.array(prev_angles)))
         if total_rotation < min_rotation:
@@ -185,3 +170,104 @@ def choose_best_ik(prev_angles, pos, rot, a_n, d_n, b, tp):
         return prev_angles
     
     return best_solution
+
+def forward_kinematics(angles, a_n, d_n, b, tp):
+        Th1 = angles[0]
+        Th2 = angles[1]
+        Th3 = angles[2]
+        Th4 = angles[3]
+        Th5 = angles[4]
+        Th6 = angles[5]
+
+        a_2 = a_n[1]
+        a_3 = a_n[2]
+        d_4 = d_n[3]
+        d_5 = d_n[4]
+
+        T_0b = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, b],
+            [0, 0, 0, 1]
+        ]
+
+        T_tp6 = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, tp],
+            [0, 0, 0, 1]
+        ]
+
+        s_1 = sin(Th1)
+        c_1 = cos(Th1)
+
+        Th01 = [
+            [c_1, -s_1, 0, 0],
+            [s_1,  c_1, 0, 0],
+            [ 0 ,   0 , 1, 0],
+            [ 0 ,   0 , 0, 1]
+        ]
+
+        s_2 = sin(Th2)
+        c_2 = cos(Th2)
+
+        Th12 = [
+            [-s_2, -c_2,  0, 0],
+            [  0 ,   0 , -1, 0],
+            [ c_2, -s_2,  0, 0],
+            [  0 ,   0 ,  0, 1]
+        ]
+
+        s_3 = sin(Th3)
+        c_3 = cos(Th3)
+
+        Th23 = [
+            [c_3, -s_3, 0, a_2],
+            [s_3,  c_3, 0,  0 ],
+            [ 0 ,   0 , 1,  0 ],
+            [ 0 ,   0 , 0,  1 ]
+        ]
+
+        s_4 = sin(Th4)
+        c_4 = cos(Th4)
+
+        Th34 = [
+            [ s_4, c_4, 0, a_3],
+            [-c_4, s_4, 0,  0 ],
+            [  0 ,  0 , 1, d_4],
+            [  0 ,  0 , 0,  1 ]
+        ]
+
+        s_5 = sin(Th5)
+        c_5 = cos(Th5)
+
+        Th45 = [
+            [ c_5, -s_5, 0,  0 ],
+            [  0 ,   0 , 1, d_5],
+            [-s_5, -c_5, 0,  0 ],
+            [  0 ,   0 , 0,  1 ]
+        ]
+
+        s_6 = sin(Th6)
+        c_6 = cos(Th6)
+
+        Th56 = [
+            [c_6, -s_6,  0, 0],
+            [ 0 ,   0 , -1, 0],
+            [s_6,  c_6,  0, 0],
+            [ 0 ,   0 ,  0, 1]
+        ]
+
+        T0 = np.dot(T_0b, Th01)
+        T1 = np.dot(T0, Th12)
+        T2 = np.dot(T1, Th23)
+        T3 = np.dot(T2, Th34)
+        T4 = np.dot(T3, Th45)
+        T5 = np.dot(T4, Th56)
+        fpk = np.dot(T5, T_tp6)
+
+        x = fpk[0][3]
+        y = fpk[1][3]
+        z = fpk[2][3]
+        hand_pos = [x, y, z]
+        return hand_pos
